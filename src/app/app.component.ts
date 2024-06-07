7764c6f0b68f46c1de656b45406548a944b659a8
Ben Leevey <Ben_Leevey@proton.me>
2024-06-07 12:00:54 -0500

    Initial Commit. App component built. Home, About and Contact components built. App component designed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,74 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'truth-frontend';
+  title = 'Our Own Hymn Book';
+  displaytitle = 'Our Own Hymn Book';
+
+  hideElement(id: string){
+    const element = document.getElementById(id);
+    if(element){
+      element.style.display = 'none';
+    } else{
+      console.error(`Element is null.`);
+    }
+  }
+  showElement(id: string){
+    const element = document.getElementById(id);
+    if(element){
+      element.style.display = 'grid';
+      } else{
+        console.error(`Element is null.`)
+      }
+  }
+  iconRotateCheck(iconId: string, id: string){
+    const element = document.getElementById(id);
+    if(element?.style.display === 'none'){
+      this.addRotate(iconId);
+    } else{
+      this.removeRotate(iconId);
+    }
+  }
+  addRotate(iconId: string){
+    const icon = document.getElementById(iconId);
+    if(icon){
+      icon.style.transform = 'rotate(180deg)';
+    }
+    else{
+      console.error(`Your element id ${iconId} seems to be null.`)
+    }
+  }
+  removeRotate(iconId: string){
+    const icon = document.getElementById(iconId);
+    if(icon){
+      icon.style.transform = 'rotate(0deg)';
+    } else{
+      console.error(`Your element id ${iconId} seems to be null.`);
+    }
+  }
+  toggleElementDisplay(id: string){
+    const element = document.getElementById(id);
+    if(element){
+      if(element?.style.display === 'none'){
+      this.showElement(id);
+      } else{
+        this.hideElement(id);
+      }
+    }  else{
+        console.error(`Element is null.`);
+      }
+  } 
+  handleMenuCollapseClick(Elementid: string, iconId: string){
+    this.toggleElementDisplay(Elementid);
+    this.iconRotateCheck(iconId, Elementid);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent},
+    { path: '', component: HomeComponent }
+];
